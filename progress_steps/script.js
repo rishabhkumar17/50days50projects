e046303444ddc0f8af36e8ae0ff6037e56354bea
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-06-01 17:21:16 +0530

    step functionality

--- progress_steps/script.js
+++ progress_steps/script.js
@@ -15,13 +15,13 @@ next.addEventListener('click', () => {
     update()
 })
 
-
-next.addEventListener('click', () => {
+prev.addEventListener('click', () => {
     currentActive--
-    
+
     if(currentActive < 1) {
         currentActive = 1
     }
+
     update()
 })
 
@@ -33,4 +33,17 @@ function update() {
             circle.classList.remove('active')
         }
     })
+
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+
+    if(currentActive === 1) {
+        prev.disabled = true
+    } else if(currentActive === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
 }
