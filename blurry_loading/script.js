cd972d5f1f2d193d572dbf5ae8735198f8a2ca5c
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-06-07 18:47:58 +0530

    load blurr effect

--- blurry_loading/script.js
+++ blurry_loading/script.js
@@ -8,6 +8,7 @@ let int = setInterval(blurring, 30)
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 }
+
 function blurring() {
     load++
 
@@ -16,5 +17,7 @@ function blurring() {
     }
 
     loadText.innerText = `${load}%`
+    loadText.style.opacity = scale(load, 0, 100, 1, 0)
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
