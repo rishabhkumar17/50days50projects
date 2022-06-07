b46ca9a783945f156720f55cfa876e98b1b2cab2
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-06-07 18:39:40 +0530

    map a range of numbers to another range of numbers #scale_function

--- blurry_loading/script.js
+++ blurry_loading/script.js
@@ -3,8 +3,11 @@ const bg = document.querySelector('.bg')
 
 let load = 0
 
-let int = setInterval(blurring, 30);
+let int = setInterval(blurring, 30)
 
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+}
 function blurring() {
     load++
 
