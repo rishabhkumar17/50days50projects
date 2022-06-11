6f81c3cdc332e5e586d3ea04cdf987be2b6d960e
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-06-11 23:51:33 +0530

    create elements

--- sound_board/script.js
+++ sound_board/script.js
@@ -1,2 +1,10 @@
+const sounds = []
 
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
 
+    btn.innerText = sound
+
+    document.getElementById('buttons').appendChild(btn)
+})
