9480ed1d2e15aaad9ce8ffe4d33797ee6c5fc228
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-06-13 23:06:34 +0530

    fetch api

--- dad_jokes/script.js
+++ dad_jokes/script.js
@@ -1,2 +1,17 @@
+ const jokeEl = document.getElementById('id')
+ const jokeBtn = document.getElementById('jokeBtn')
 
+ generateJoke()
+
+ function generateJoke() {
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        },
+    }
+
+    fetch('https://icanhazdadjoke.com', config)
+    .then(res => res.json())
+    .then(data => console.log(data))
+ }
 
