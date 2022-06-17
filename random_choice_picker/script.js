4cc88d65cddd2f10ada60710da3f96330f67480e
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-06-17 23:01:02 +0530

    focus

--- random_choice_picker/script.js
+++ random_choice_picker/script.js
@@ -1,2 +1,13 @@
+const tagsEl = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
 
+textarea.focus()
+
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value)
+})
+
+function createTags(input) {
+    console.log(input)
+}
 
