407e6567a942a44a360f636bf3417ac074f36235
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-06-17 23:14:00 +0530

    create tags

--- random_choice_picker/script.js
+++ random_choice_picker/script.js
@@ -1,3 +1,4 @@
+
 const tagsEl = document.getElementById('tags')
 const textarea = document.getElementById('textarea')
 
@@ -5,9 +6,26 @@ textarea.focus()
 
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
+
+    if(e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10)
+
+        randomSelect()
+    }
 })
 
 function createTags(input) {
-    console.log(input)
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+    
+    tagsEl.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagEl)
+    })
 }
 
