c365608ec3540b433d7b196ee49b9e21451020f8
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-06-21 23:01:37 +0530

    select random tag

--- random_choice_picker/script.js
+++ random_choice_picker/script.js
@@ -40,6 +40,16 @@ function randomSelect() {
             unHighlightTag(randomTag)
         }, 100)
     }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        }, 100)
+    }, times * 100)
 }
 
 function pickRandomTag() {
